1056cbf2456a7c83aef2f1cf803c6ffc3cc05622
arajni3 <fancypirate10@gmail.com>
2022-06-23 20:24:58 -0400

    imported useState in purchaseHistory.js

--- frontend/src/components/purchaseHistory.js
+++ frontend/src/components/purchaseHistory.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import {BrowserRouter, Navigate} from 'react-router-dom';
 import {getShoppingItems, getTotal} from "../helper-functions/otherCartOperations.js";
 import axiosInstance from "../httpRequests.js";
