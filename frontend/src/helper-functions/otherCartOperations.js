da9b8c9a9d2f1eb22e13f46c4d83a3e63157ac19
arajni3 <fancypirate10@gmail.com>
2022-09-15 01:16:57 -0400

    fixed dependency problem in otherCartOperations.js

--- frontend/src/helper-functions/otherCartOperations.js
+++ frontend/src/helper-functions/otherCartOperations.js
@@ -1,15 +1,16 @@
-import shoppingSelections from "../constants/shoppingSelections.js";
+import axiosAWS from "../httpRequests.js";
 
-export const getShoppingItems = (selections) => {
+export const getShoppingItems = async (selections) => {
+    let shoppingSelections = (await axiosAWS.get("imageData")).data.imageData;
     let obj = {};
     for (let i = 0; i < selections.length; ++i) {
         for (let j = 0; j < shoppingSelections.length; ++j) {
             let curType = selections[i];
-            if (curType === shoppingSelections[j].type) {
+            if (curType === shoppingSelections[j]['image_description']) {
                 if (obj.hasOwnProperty(curType)) {
                     obj[curType].count++;
                 } else {
-                    obj[curType] = ({type: curType, cost: shoppingSelections[j].cost, count: 1});
+                    obj[curType] = ({type: curType, cost: shoppingSelections[j]['cost'], count: 1});
                 }
             }
         }
